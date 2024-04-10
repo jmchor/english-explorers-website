1f1da8b903dda26909ac7fd9fd28990609a969fc
Johannes Chorzempa <jchorzempa@posteo.org>
2024-04-10 19:20:08 +0200

    Add controlled accordions

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -3,6 +3,7 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import { Icon } from '@iconify/react';
 import styled from 'styled-components';
+import React from 'react';
 
 const CustomAccordionSummary = styled(AccordionSummary)`
 	justify-content: center;
@@ -12,20 +13,76 @@ const CustomAccordionSummary = styled(AccordionSummary)`
 	}
 `;
 
-const AccordionComponent = ({ summary, details }: { summary: string; details: string }) => {
+const AccordionComponent = () => {
+	const [expanded, setExpanded] = React.useState<string | false>(false);
+
+	const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+		setExpanded(isExpanded ? panel : false);
+	};
 	return (
-		<Accordion>
-			<CustomAccordionSummary
-				expandIcon={<Icon icon='uiw:caret-down' />}
-				aria-controls='panel1-content'
-				id='panel1-header'
-			>
-				<p>{summary}</p>
-			</CustomAccordionSummary>
-			<AccordionDetails>
-				<p>{details}</p>
-			</AccordionDetails>
-		</Accordion>
+		<>
+			<Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+				<CustomAccordionSummary
+					expandIcon={<Icon icon='uiw:caret-down' />}
+					aria-controls='panel1-content'
+					id='panel1-header'
+				>
+					<p>"Who is there, and who can join?"</p>
+				</CustomAccordionSummary>
+				<AccordionDetails>
+					<p>
+						Currently, we have an average of 10 kids between 3 and 10 years of age, and we are welcoming families with
+						children 0-10 who speak English at home with at least one parent.
+					</p>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+				<CustomAccordionSummary
+					expandIcon={<Icon icon='uiw:caret-down' />}
+					aria-controls='panel1-content'
+					id='panel1-header'
+				>
+					<p>"Is this for free?"</p>
+				</CustomAccordionSummary>
+				<AccordionDetails>
+					<p>
+						We do not require any money for admission, but for material and such we ask every attending family to pay 25
+						Euro for six months.
+					</p>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+				<CustomAccordionSummary
+					expandIcon={<Icon icon='uiw:caret-down' />}
+					aria-controls='panel1-content'
+					id='panel1-header'
+				>
+					<p>"What do you do exactly?"</p>
+				</CustomAccordionSummary>
+				<AccordionDetails>
+					<p>
+						Our activities include singing together and story time, as well as arts and crafts and other games. We try
+						to make it as inclusive and engaging as possible for everybody, which for example includes grouping children
+						for more age-appropriate activities.
+					</p>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+				<CustomAccordionSummary
+					expandIcon={<Icon icon='uiw:caret-down' />}
+					aria-controls='panel1-content'
+					id='panel1-header'
+				>
+					<p>"Can I help?"</p>
+				</CustomAccordionSummary>
+				<AccordionDetails>
+					<p>
+						Absolutely you can help! We appreciate every support - even if it is "just" for the cleanup afterwards.
+						Please contact us so we can talk more.
+					</p>
+				</AccordionDetails>
+			</Accordion>
+		</>
 	);
 };
 export default AccordionComponent;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -33,23 +33,7 @@ export default function About() {
 					opportunity to speak their native language with other kids.
 				</p>
 				<AccordionContainer>
-					<AccordionComponent
-						summary='"Who is there, and who can join?"'
-						details='Currently, we have an average of 10 kids between 3 and 10 years of age, and we are welcoming families with
-								children 0-10 who speak English at home with at least one parent.'
-					/>
-					<AccordionComponent
-						summary='"Is this for free?"'
-						details='We do not require any money for admission, but for material and such we ask every attending family to pay 25 Euro for six months.'
-					/>
-					<AccordionComponent
-						summary='"What do you do exactly?"'
-						details='Our activities include singing together and story time, as well as arts and crafts and other games. We try to make it as inclusive and engaging as possible for everybody, which for example includes grouping children for more age-appropriate activities.'
-					/>
-					<AccordionComponent
-						summary='"Can I help?"'
-						details='Absolutely you can help! We appreciate every support - even if it is "just" for the cleanup afterwards. Please contact us so we can talk more.'
-					/>
+					<AccordionComponent />
 				</AccordionContainer>
 			</Aboutcontainer>
 		</HomeContainer>
