263d7545915ef88a3a5aa8328816685d440bcefe
Johannes Chorzempa <jchorzempa@posteo.org>
2024-04-10 06:41:10 +0200

    Add OpenSans and global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import RootLayout from '@/components/RootLayout';
+import { GlobalStyles } from '@/styles/GlobalStyles';
+import type { AppProps } from 'next/app';
+import { Open_Sans } from 'next/font/google';
+
+const openSans = Open_Sans({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<main className={openSans.className}>
+			<GlobalStyles />
+
+			<RootLayout>
+				<Component {...pageProps} />
+			</RootLayout>
+		</main>
+	);
 }
