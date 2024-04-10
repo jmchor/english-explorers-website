bf1156812ab4c642ac70d69fc24d107a0ce736c6
Johannes Chorzempa <jchorzempa@posteo.org>
2024-04-10 10:01:50 +0200

    aad Accordion component to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
 import { HomeContainer } from './home';
 import styled from 'styled-components';
+import Accordion from '@mui/material/Accordion';
+import AccordionComponent from '@/components/Accordion';
 
 const Aboutcontainer = styled.div`
 	display: flex;
@@ -13,21 +15,42 @@ const Aboutcontainer = styled.div`
 	}
 `;
 
+const AccordionContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: 5rem;
+`;
+
 export default function About() {
 	return (
 		<HomeContainer>
 			<Aboutcontainer>
 				<h1>About</h1>
-				<p>English Explorers was formed in the 17th century as a side arm of the free masons.</p>
 				<p>
-					<b>"Who is there, and who can join?"</b>
+					English Explorers was formed in 2013 by a group of friends who wanted to give their kids the opportunity to
+					speak their native language with other kids.
 				</p>
-				<p>
-					Currently, we have an average of 10 kids between 3 and 10 years of age, and we are welcoming families with
-					children 0-10 who speak English at home with at least one parent.
-				</p>
-				<p>"Does this cost anything?"</p>
-				<p>Yes, No, Yes - well.</p>
+				<AccordionContainer>
+					<AccordionComponent
+						summary='"Who is there, and who can join?"'
+						details='Currently, we have an average of 10 kids between 3 and 10 years of age, and we are welcoming families with
+								children 0-10 who speak English at home with at least one parent.'
+					/>
+					<AccordionComponent
+						summary='"Is this for free?"'
+						details='We do not require any money for admission, but for material and such we ask every attending family to pay 25 Euro for six months.'
+					/>
+					<AccordionComponent
+						summary='"What do you do exactly?"'
+						details='Our activities include singing together and story time, as well as arts and crafts and other games. We try to make it as inclusive and engaging as possible for everybody, which for example includes grouping children for more age-appropriate activities.'
+					/>
+					<AccordionComponent
+						summary='"Can I help?"'
+						details='Absolutely you can help! We appreciate every support - even if it is "just" for the cleanup afterwards. Please contact us so we can talk more.'
+					/>
+				</AccordionContainer>
 			</Aboutcontainer>
 		</HomeContainer>
 	);
